88a6fcd0ef0b72ca11b2b5aadf024d4c4f387d28
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-03-27 02:20:49 +0530

    trying to iterate through the list to find the streaks //index going out of range

--- coin_flip_streaks.py
+++ coin_flip_streaks.py
@@ -11,12 +11,33 @@ def flip(number):
             flip_list.append('T')
     return flip_list
 
-# def streak_checker(obs_set):
-#     for i in range(len(obs_set)):
+def streak_checker(obs_set):
+    x = 0
+    for i in range(len(obs_set)):
+        if obs_set[i]== obs_set[i+1]:
+            if obs_set[i+1] == obs_set[i+2]:
+                if obs_set[i+2] == obs_set[i+3]:
+                    if obs_set[i+3] == obs_set[i+4]:
+                        if obs_set[i+4] == obs_set[i+5]:
+                            print('its a streak')
+                            x = x+1
+                        else:
+                            continue
+                    else:
+                        continue
+                else:
+                    continue
+            else:
+                continue
+        else:
+            continue
+    return x
+
 
 
 print('Enter the size of your observation set:')
 observation_number = input()
 gen_list = flip(observation_number)
 print(gen_list)
-# streak_checker(gen_list)
+number_streaks = streak_checker(gen_list)
+print(number_streaks)
